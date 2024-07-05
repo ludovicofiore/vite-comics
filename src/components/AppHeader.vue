4b0d9ce61219c62a2360ea344a27e9bb554aaccd
ludoff <ludovicofiore25@gmail.com>
2024-07-05 19:24:26 +0200

    feat: add header nav dinamically, add hover and active functions

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,64 @@
 <script>
     export default {
         name: 'AppHeader',
+
+        // aggiungo struttura dati
+        data() {
+            return {
+                links: [
+                    {
+                        text: "CHARACTERS",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "COMICS",
+                        url: "#",
+                        current: true
+                    },
+                    {
+                        text: "MOVIES",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "TV",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "GAMES",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "COLLECTIBLES",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "VIDEOS",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "FANS",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "NEWS",
+                        url: "#",
+                        current: false
+                    },
+                    {
+                        text: "SHOP",
+                        url: "#",
+                        current: false
+                    },
+                ]
+            }
+        }
     }
 </script>
 
@@ -11,37 +69,13 @@
             <span id="header-logo">
                 <img src="../assets/img/dc-logo.png" alt="logo DC">
             </span>
+
             <ul>
-                <li>
-                    <a href="#">COMICS</a>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :href="link.url" :class="link.current ? 'active' : ''">{{ link.text }}</a>
+                    
                 </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
-                <li>
-                    <a href="#">COMICS</a>
-                </li>
+               
             </ul>
         </div>
        
@@ -49,15 +83,17 @@
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
 header {
     background-color: white;
+    height: 150px;
 
 }
 
 .row {
 
-    
+    height: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -65,6 +101,24 @@ header {
     ul {
         display: flex;
         gap: 15px;
+        height: 100%;
+
+        a {
+            display: inline-block;
+            color: black;
+            font-weight: bold;
+            height: 100%;
+            padding-top: 70px;
+
+            &.active,
+            &:hover {
+                color: $dc-blue;
+                border-bottom: 5px solid $dc-blue;
+            }
+
+        
+        }
+
 
     }
 }
