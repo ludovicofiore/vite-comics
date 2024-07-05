a6b9744ce56c3191f2f0d4b207abbd162b294092
ludoff <ludovicofiore25@gmail.com>
2024-07-05 16:06:23 +0200

    feat: add header structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
 </script>
 
 <template>
-  
+  <AppHeader />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,71 @@
+<script>
+    export default {
+        name: 'AppHeader',
+    }
+</script>
+
+<template>
+    <header>
+        <div class="row">
+
+            <span id="header-logo">
+                <img src="../assets/img/dc-logo.png" alt="logo DC">
+            </span>
+            <ul>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+            </ul>
+        </div>
+       
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    background-color: white;
+
+}
+
+.row {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    ul {
+        display: flex;
+        gap: 15px;
+
+    }
+}
+</style>
