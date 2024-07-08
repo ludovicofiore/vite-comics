169d7f74d2915db67c71bbce0b2095b6b324564c
ludoff <ludovicofiore25@gmail.com>
2024-07-08 15:48:27 +0200

    feat: add component for cards and first static card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        name: 'AppCard'
+    }
+</script>
+
+
+<template>
+    <div class="card">
+        <div class="card-image">
+            <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        </div>
+
+        <div class="card-text">
+            <span>action comics</span>
+        </div>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+.card {
+    width: calc(100% / 6);
+
+    .card-image img {
+        max-width: 100%;
+    }
+
+    .card-text {
+        margin-top: 20px;
+    }
+}
+</style>
